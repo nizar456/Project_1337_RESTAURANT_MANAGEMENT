f0a7e25f42e87aae261b2556e964d9b8eb1300fb
l9irche <153326705+l9irche@users.noreply.github.com>
2024-07-19 13:03:56 +0100

    admin command

--- html/Commands.php
+++ html/Commands.php
@@ -13,7 +13,6 @@ header("Expires: 0"); // Proxies
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 
-
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -30,102 +29,55 @@ header("Expires: 0"); // Proxies
   <link rel="icon" type="image/png" sizes="16x16" href="plugins/images/favicon.png" />
   <!-- Custom CSS -->
   <link href="css/style.min.css" rel="stylesheet" />
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <style>
+.command-card {
+    border: 1px solid #ddd;
+    padding: 15px;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+.command-card h5 {
+    margin-top: 0;
+}
+</style>
 </head>
 
 <body>
-  <!-- ============================================================== -->
-  <!-- Preloader - style you can find in spinners.css -->
-  <!-- ============================================================== -->
   <div class="preloader">
     <div class="lds-ripple">
       <div class="lds-pos"></div>
       <div class="lds-pos"></div>
     </div>
   </div>
-  <!-- ============================================================== -->
-  <!-- Main wrapper - style you can find in pages.scss -->
-  <!-- ============================================================== -->
   <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
     data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
-    <!-- ============================================================== -->
-    <!-- Topbar header - style you can find in pages.scss -->
-    <!-- ============================================================== -->
     <header class="topbar" data-navbarbg="skin5">
       <nav class="navbar top-navbar navbar-expand-md navbar-dark">
         <div class="navbar-header" data-logobg="skin6">
-          <!-- ============================================================== -->
-          <!-- Logo -->
-          <!-- ============================================================== -->
-          <a class="navbar-brand" href="index.html">
-            <!-- Logo icon -->
+          <a class="navbar-brand" href="index.php">
             <b class="logo-icon">
-              <!-- Dark Logo icon -->
               <img src="plugins/images/logo-icon.png" alt="homepage" />
             </b>
-            <!--End Logo icon -->
-            <!-- Logo text -->
             <span class="logo-text">
-              <!-- dark Logo text -->
               <img src="plugins/images/logo-text.png" alt="homepage" />
             </span>
           </a>
-          <!-- ============================================================== -->
-          <!-- End Logo -->
-          <!-- ============================================================== -->
-          <!-- ============================================================== -->
-          <!-- toggle and nav items -->
-          <!-- ============================================================== -->
-          <a class="
-                nav-toggler
-                waves-effect waves-light
-                text-dark
-                d-block d-md-none
-              " href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+          <a class="nav-toggler waves-effect waves-light text-dark d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
         </div>
-        <!-- ============================================================== -->
-        <!-- End Logo -->
-        <!-- ============================================================== -->
         <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
-          <!-- ============================================================== -->
-          <!-- Right side toggle and nav items -->
-          <!-- ============================================================== -->
           <ul class="navbar-nav ms-auto d-flex align-items-center">
-            <!-- ============================================================== -->
-            <!-- Search -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- User profile and search -->
-            <!-- ============================================================== -->
             <li>
-              <span
-                  class="text-white font-medium">Bonjour <?php echo htmlspecialchars($_SESSION['user_name']); ?>   </span>
+              <span class="text-white font-medium">Bonjour <?php echo htmlspecialchars($_SESSION['user_name']); ?></span>
             </li>
-            <!-- ============================================================== -->
-            <!-- User profile and search -->
-            <!-- ============================================================== -->
           </ul>
         </div>
       </nav>
     </header>
-    <!-- ============================================================== -->
-    <!-- End Topbar header -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Left Sidebar - style you can find in sidebar.scss  -->
-    <!-- ============================================================== -->
     <aside class="left-sidebar" data-sidebarbg="skin6">
-      <!-- Sidebar scroll-->
       <div class="scroll-sidebar">
-        <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
           <ul id="sidebarnav">
-            <!-- User Profile-->
             <li class="sidebar-item pt-2">
               <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index.php" aria-expanded="false">
                 <i class="far fa-clock" aria-hidden="true"></i>
@@ -139,8 +91,7 @@ header("Expires: 0"); // Proxies
               </a>
             </li>
             <li class="sidebar-item">
-              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="Commands.php"
-                aria-expanded="false">
+              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="Commands.php" aria-expanded="false">
                 <i class="fa fa-table" aria-hidden="true"></i>
                 <span class="hide-menu">Commands</span>
               </a>
@@ -159,83 +110,83 @@ header("Expires: 0"); // Proxies
             </li>
           </ul>
         </nav>
-        <!-- End Sidebar navigation -->
       </div>
-      <!-- End Sidebar scroll-->
     </aside>
-    <!-- ============================================================== -->
-    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- Page wrapper  -->
-    <!-- ============================================================== -->
     <div class="page-wrapper">
-      <!-- ============================================================== -->
-      <!-- Bread crumb and right sidebar toggle -->
-      <!-- ============================================================== -->
       <div class="page-breadcrumb bg-white">
         <div class="row align-items-center">
           <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
             <h4 class="page-title">Commands</h4>
           </div>
         </div>
-        <!-- /.col-lg-12 -->
       </div>
-      <!-- ============================================================== -->
-      <!-- End Bread crumb and right sidebar toggle -->
-      <!-- ============================================================== -->
-      <!-- ============================================================== -->
-      <!-- Container fluid  -->
-      <!-- ============================================================== -->
       <div class="container-fluid">
-        <!-- ============================================================== -->
-        <!-- Start Page Content -->
-        <!-- ============================================================== -->
+        <?php
+        require_once 'db_connection.php';
         
-        <!-- ============================================================== -->
-        <!-- End PAge Content -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Right sidebar -->
-        <!-- ============================================================== -->
-        <!-- .right-sidebar -->
-        <!-- ============================================================== -->
-        <!-- End Right sidebar -->
-        <!-- ============================================================== -->
+        // Get current date
+        $currentDate = date('Y-m-d');
+        
+        // Query to get commands for the current day that are not done
+        $query = "SELECT c.id, c.date, c.table_num, c.etudiant_id, c.done, e.nom AS etudiant_name
+                  FROM command c
+                  JOIN etudiant e ON c.etudiant_id = e.id
+                  WHERE c.date = ? AND c.done = 0";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('s', $currentDate);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo '<div class="command-card">';
+                echo '<h5>Command ID: ' . $row['id'] . '</h5>';
+                echo '<p>Date: ' . $row['date'] . '</p>';
+                echo '<p>Table Number: ' . $row['table_num'] . '</p>';
+                echo '<p>Student Name: ' . $row['etudiant_name'] . '</p>';
+                
+                // Query to get products for the current command
+                $commandId = $row['id'];
+                $productQuery = "SELECT p.nom AS product_name, c.quantity
+                                 FROM contains c
+                                 JOIN products p ON c.product_id = p.id
+                                 WHERE c.command_id = ?";
+                $productStmt = $conn->prepare($productQuery);
+                $productStmt->bind_param('i', $commandId);
+                $productStmt->execute();
+                $productResult = $productStmt->get_result();
+                
+                echo '<ul>';
+                while ($productRow = $productResult->fetch_assoc()) {
+                    echo '<li>' . $productRow['product_name'] . ' - Quantity: ' . $productRow['quantity'] . '</li>';
+                }
+                echo '</ul>';
+                
+                echo '<form method="POST" action="validate_command.php">';
+                echo '<input type="hidden" name="command_id" value="' . $row['id'] . '">';
+                echo '<button type="submit" class="btn btn-success">Validate Command</button>';
+                echo '</form>';
+                
+                echo '</div>';
+            }
+        } else {
+            echo '<p>No commands for today.</p>';
+        }
+        
+        $stmt->close();
+        $conn->close();
+        ?>
       </div>
-      <!-- ============================================================== -->
-      <!-- End Container fluid  -->
-      <!-- ============================================================== -->
-      <!-- ============================================================== -->
-      <!-- footer -->
-      <!-- ============================================================== -->
       <footer class="footer text-center">
-        2024 © Ample Admin brought to you by
-        <a href="https://www.wrappixel.com/">wrappixel.com</a>
+        2024 © 1337
       </footer>
-      <!-- ============================================================== -->
-      <!-- End footer -->
-      <!-- ============================================================== -->
     </div>
-    <!-- ============================================================== -->
-    <!-- End Page wrapper  -->
-    <!-- ============================================================== -->
   </div>
-  <!-- ============================================================== -->
-  <!-- End Wrapper -->
-  <!-- ============================================================== -->
-  <!-- ============================================================== -->
-  <!-- All Jquery -->
-  <!-- ============================================================== -->
   <script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
-  <!-- Bootstrap tether Core JavaScript -->
   <script src="bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <script src="js/app-style-switcher.js"></script>
-  <!--Wave Effects -->
   <script src="js/waves.js"></script>
-  <!--Menu sidebar -->
   <script src="js/sidebarmenu.js"></script>
-  <!--Custom JavaScript -->
   <script src="js/custom.js"></script>
 </body>
 
