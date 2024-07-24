f1d08f045cf82b70cd0ff0170ff047a80b08cb6a
l9irche <153326705+l9irche@users.noreply.github.com>
2024-07-24 10:25:48 +0100

    l9asse

--- html/add_product.php
+++ html/add_product.php
@@ -33,10 +33,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Allow certain file formats
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+   /* if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
         $response['message'] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
         $uploadOk = 0;
-    }
+    }*/
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
